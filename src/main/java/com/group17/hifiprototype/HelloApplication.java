79d32038d6b3346dac72610d87b8c6093f0f6ac3
xt0r3 <kgs37@cam.ac.uk>
2022-05-10 16:10:07 +0100

    [GUI General] Added some responsive buttons to the main page.

--- src/main/java/com/group17/hifiprototype/HelloApplication.java
+++ src/main/java/com/group17/hifiprototype/HelloApplication.java
@@ -3,6 +3,7 @@ package com.group17.hifiprototype;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,22 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        // SET STAGE PARAMETERS
+        stage.setTitle("WeatherAce");
+        /*
+         TODO: add icon to resources folder and uncomment
+         Image icon = new Image("icon.png");
+         stage.getIcons().add(icon);
+
+         stage.setWidth(500);  // can be resized during runtime
+         stage.setHeight(1300);  // can be resized during runtime
+         */
+
+        // LOAD APPLICATION PAGES
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        scene.getStylesheets().add("style.css");
+
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/group17/hifiprototype/HelloController.java
+++ src/main/java/com/group17/hifiprototype/HelloController.java
@@ -2,13 +2,16 @@ package com.group17.hifiprototype;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private VBox contentWindow;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText(Double.toString(contentWindow.getWidth()));
     }
 }
