56a468e4a888e631a31ba2e7b840fa8de59a7daa
xt0r3 <kgs37@cam.ac.uk>
2022-05-10 14:37:50 +0100

    [GUI General] First commit, initialized repository

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.group17.hifiprototype {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.group17.hifiprototype to javafx.fxml;
+    exports com.group17.hifiprototype;
+}
