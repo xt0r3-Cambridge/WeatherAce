6f3b0503fd55a85977a82e506fb44eeec526fec8
Alexandru Trifanov <at979@cam.ac.uk>
2022-05-12 15:58:38 +0100

    Added Granularity enum. Added visibility field to DataPoint. Started adding Session.

--- src/main/java/com/group17/hifiprototype/dataclasses/DataPoint.java
+++ src/main/java/com/group17/hifiprototype/dataclasses/DataPoint.java
@@ -5,17 +5,18 @@ import java.util.Date;
 public class DataPoint {
     private final double latitude, longitude;
     private final Date time;
-    private final double airTemperature;
-    private final Precipitation precipitation;
+    private double airTemperature;
+    private Precipitation precipitation;
     private double groundTemperature;
     private double humidity;
     private double precipitationChance;
     private double windSpeed;
     private double windDirection;
+    private double visibility;
 
     public DataPoint(double latitude, double longitude, Date time, double airTemperature,
                      Precipitation precipitation, double groundTemperature, double humidity,
-                     double precipitationChance, double windSpeed, double windDirection) {
+                     double precipitationChance, double windSpeed, double windDirection,double visibility) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.time = time;
@@ -26,6 +27,13 @@ public class DataPoint {
         this.precipitationChance = precipitationChance;
         this.windSpeed = windSpeed;
         this.windDirection = windDirection;
+        this.visibility = visibility;
+    }
+
+    public DataPoint(double latitude, double longitude, Date time) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.time = time;
     }
 
     public double getLatitude() {
@@ -67,6 +75,43 @@ public class DataPoint {
     public Precipitation getPrecipitation() {
         return precipitation;
     }
+
+    public double getVisibility() {
+        return visibility;
+    }
+
+    public void setAirTemperature(double airTemperature) {
+        this.airTemperature = airTemperature;
+    }
+
+    public void setPrecipitation(Precipitation precipitation) {
+        this.precipitation = precipitation;
+    }
+
+    public void setGroundTemperature(double groundTemperature) {
+        this.groundTemperature = groundTemperature;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setPrecipitationChance(double precipitationChance) {
+        this.precipitationChance = precipitationChance;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public void setWindDirection(double windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public void setVisibility(double visibility) {
+        this.visibility = visibility;
+    }
+
 
 
 }
---
+++ src/main/java/com/group17/hifiprototype/dataclasses/Granularity.java
@@ -0,0 +1,5 @@
+package com.group17.hifiprototype.dataclasses;
+
+public enum Granularity {
+    Minutes,Hours,Days;
+}
