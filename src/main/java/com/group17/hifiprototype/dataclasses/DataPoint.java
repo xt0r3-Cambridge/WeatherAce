2befec5d34ffbe5bf4373c84e9dc1330e7d9786c
Alexandru Trifanov <at979@cam.ac.uk>
2022-05-12 15:21:15 +0100

    Added Precipitation enum and integrated it into DataPoint.

--- src/main/java/com/group17/hifiprototype/dataclasses/DataPoint.java
+++ src/main/java/com/group17/hifiprototype/dataclasses/DataPoint.java
@@ -3,20 +3,29 @@ package com.group17.hifiprototype.dataclasses;
 import java.util.Date;
 
 public class DataPoint {
-    private final double latitude,longitude;
+    private final double latitude, longitude;
     private final Date time;
-    private double airTemperature;
-
+    private final double airTemperature;
+    private final Precipitation precipitation;
     private double groundTemperature;
     private double humidity;
     private double precipitationChance;
     private double windSpeed;
     private double windDirection;
 
-    public DataPoint(double latitude, double longitude, Date time) {
-        this.latitude=latitude;
-        this.longitude=longitude;
-        this.time=time;
+    public DataPoint(double latitude, double longitude, Date time, double airTemperature,
+                     Precipitation precipitation, double groundTemperature, double humidity,
+                     double precipitationChance, double windSpeed, double windDirection) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.time = time;
+        this.airTemperature = airTemperature;
+        this.precipitation = precipitation;
+        this.groundTemperature = groundTemperature;
+        this.humidity = humidity;
+        this.precipitationChance = precipitationChance;
+        this.windSpeed = windSpeed;
+        this.windDirection = windDirection;
     }
 
     public double getLatitude() {
@@ -53,6 +62,10 @@ public class DataPoint {
 
     public double getWindDirection() {
         return windDirection;
+    }
+
+    public Precipitation getPrecipitation() {
+        return precipitation;
     }
 
 
