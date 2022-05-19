e4d6c5ef8a1501ec11ef6a3d345aeb4aaba45d90
kwt31 <kwt31@cam.ac.uk>
2022-05-19 15:28:16 +0100

    updated wec controllers

--- src/main/java/com/group17/hifiprototype/scenes/wec_scene/wec_monza/WecMonzaController.java
+++ src/main/java/com/group17/hifiprototype/scenes/wec_scene/wec_monza/WecMonzaController.java
@@ -1,22 +1,46 @@
 package com.group17.hifiprototype.scenes.wec_scene.wec_monza;
 
+import com.group17.hifiprototype.dataclasses.*;
 import com.group17.hifiprototype.scenes.base_scene.BaseMotorsportController;
 import com.group17.hifiprototype.scenes.utils.GridControls;
 import com.group17.hifiprototype.scenes.utils.SceneController;
 import com.group17.hifiprototype.scenes.utils.SceneId;
 import com.group17.hifiprototype.scenes.utils.VBoxControls;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WecMonzaController extends BaseMotorsportController {
 
     public void loadWeatherData(){
-        GridControls.newRowFromData(super.detailedDataContainer, "FRI", "", "", "", "", "");
-        GridControls.newRowFromData(super.detailedDataContainer, "14:20", "12%", "36°C", "200°C", "700km/h", "1.5m");
-        GridControls.newRowFromData(super.detailedDataContainer, "14:20", "12%", "36°C", "200°C", "700km/h", "1.5m");
-        GridControls.newRowFromData(super.detailedDataContainer, "14:20", "12%", "36°C", "200°C", "700km/h", "1.5m");
-        GridControls.newRowFromData(super.detailedDataContainer, "14:20", "12%", "36°C", "200°C", "700km/h", "1.5m");
-        GridControls.newRowFromData(super.detailedDataContainer, "14:20", "12%", "36°C", "200°C", "700km/h", "1.5m");
-        GridControls.newRowFromData(super.detailedDataContainer, "14:20", "12%", "36°C", "200°C", "700km/h", "1.5m");
+        try {
+            Race currentRace = RaceLoader.getGroup(RaceGroups.F1).getRace("6 Hours of Monza");
+            currentRace.loadWeatherData();
+            System.out.println("HERE");
 
+            ArrayList<Session> sessions = currentRace.getSessions();
+
+            for (Session session : sessions) {
+                GridControls.newRowFromData(super.detailedDataContainer, session.getName(), "", "", "", "", "");
+                System.out.println(session.getName());
+                List<DataPoint> dataPointList = session.getDataPoints();
+                for (DataPoint dataPoint : dataPointList) {
+                    GridControls.newRowFromData(
+                            super.detailedDataContainer,
+                            dataPoint.prettyTime(),
+                            dataPoint.prettyPrecipitation(),
+                            dataPoint.prettyAirTemperature(),
+                            dataPoint.prettyGroundTemperature(),
+                            dataPoint.prettyWind(),
+                            dataPoint.prettyVisibility()
+                    );
+                }
+                GridControls.newRowFromData(super.detailedDataContainer, "", "", "", "", "", "");
+            }
+        } catch (Exception ignored){
+            GridControls.newRowFromData(super.detailedDataContainer, "ERROR:", "Race at", "an invalid", "time", "Filled with", "mock data");
+            GridControls.fillWithMock(super.detailedDataContainer);
+        }
     }
 
     public void loadRaceData(){
@@ -31,9 +55,19 @@ public class WecMonzaController extends BaseMotorsportController {
          * Sets the main weather data for the non-scrollable box in the middle of the screen
          * Needs to be implemented
          */
-        super.mainWind.setText("75km/h ↗");
-        super.mainRain.setText("99%");
-        super.mainTemp.setText("658°C");
+        try {
+            Race currentRace = RaceLoader.getGroup(RaceGroups.F1).getRace("6 Hours of Monza");
+            currentRace.loadWeatherData();
+            DataPoint mainDataPoint = currentRace.getMainDataPoint();
+
+            super.mainWind.setText(mainDataPoint.prettyWind());
+            super.mainRain.setText(mainDataPoint.prettyAirTemperature());
+            super.mainTemp.setText(mainDataPoint.prettyGroundTemperature());
+        } catch (Exception ignored){
+            super.mainWind.setText("4 m/s " + Direction.NE);
+            super.mainRain.setText("36°C");
+            super.mainTemp.setText("42°C");
+        }
     }
 }
 
